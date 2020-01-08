aaa27a622270ebf499d430efc5211ce11016e95b
Your Name <you@example.com>
2020-01-08 08:32:44 +0100

    ...

---
+++ lidar_latifi.R
@@ -0,0 +1,17 @@
+### Lidar Course Latifi
+
+install.packages("lidR")
+install.packages("rlas")
+install.packages("rLiDAR")
+install.packages("alphashape3d")
+install.packages("geometry")
+
+
+library(raster)
+library(sp)
+library(lidR)
+library(rlas)
+library(rLiDAR)
+library(alphashape3d)
+library(geometry)
+library(rgl)
